d26381c0605934e45cf7c5c946ed19f2bd95b089
Jacob Bowdoin <jwbowdoin@gmail.com>
2018-10-18 22:13:17 -0700

    Added styles, installed Angular material

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -10,26 +10,116 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-admin.initializeApp();
+// Service account required for accessing public download URL
+const serviceAccount = require('../credentials.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://read-books-to-me.firebaseio.com'
+});
+// Cloud Storage
+const Storage = require("@google-cloud/storage");
+const gcs = new Storage();
+const os_1 = require("os");
+const path_1 = require("path");
+const fs = require("fs");
+const fse = require("fs-extra");
 // Cloud Vision
 const vision = require("@google-cloud/vision");
 const visionClient = new vision.ImageAnnotatorClient();
+// Cloud Text to Speech
+const textToSpeech = require("@google-cloud/text-to-speech");
+const client = new textToSpeech.TextToSpeechClient();
 exports.textExtraction = functions.storage
     .object()
     .onFinalize((object) => __awaiter(this, void 0, void 0, function* () {
+    if (!object.contentType.includes('image')) {
+        console.log('exiting function');
+        return false;
+    }
     const fileBucket = object.bucket;
     const filePath = object.name;
+    const pageName = filePath.split('/').pop();
+    const bucketDir = path_1.dirname(filePath);
     const imageUri = `gs://${fileBucket}/${filePath}`;
-    const docId = filePath.split('.jpg')[0];
-    // const docRef  = admin.firestore().collection('photos').doc(docId);
+    const docRef = admin.firestore().collection('pages');
     // Text Extraction
     const textRequest = yield visionClient.documentTextDetection(imageUri);
     const fullText = textRequest[0].textAnnotations[0];
     const text = fullText ? fullText.description : null;
-    console.log(text);
-    // Save to Firestore
-    // const data = { text, web, faces, landmarks }
-    return;
-    // return docRef.set(text);
+    // Construct the text-to-speech request
+    const request = {
+        input: { text: text },
+        // Select the language and SSML Voice Gender (optional)
+        voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+        // Select the type of audio encoding
+        audioConfig: { audioEncoding: 'MP3' },
+    };
+    console.log('finished constructing the text-so-speech request:', request);
+    // Creat temp directory
+    const workingDir = path_1.join(os_1.tmpdir(), 'synthesized');
+    const tmpFilePath = path_1.join(workingDir, 'output.mp3');
+    // Ensure temp directory exists
+    yield fse.ensureDir(workingDir);
+    // Turn fs.writeFile into a Promise
+    const writeFilePromise = (file, data, option) => {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(file, data, option, error => {
+                if (error)
+                    reject(error);
+                resolve("File created! Time for the next step!");
+            });
+        });
+    };
+    // Performs the Text-to-Speech request
+    const audioName = `${pageName}.mp3`;
+    let audioPath = 'notRetrieved';
+    console.log('about to start the speech synthesizer');
+    yield client.synthesizeSpeech(request)
+        .then(responses => {
+        const response = responses[0];
+        console.log('audio response: ', response.audioContent);
+        return writeFilePromise(tmpFilePath, response.audioContent, 'binary');
+    })
+        .then(() => {
+        return gcs.bucket(fileBucket).upload(tmpFilePath, {
+            destination: path_1.join(bucketDir, audioName)
+        });
+    })
+        .then(() => {
+        console.log('audio uploaded successfully');
+        const updloadedAudioFile = gcs.bucket(fileBucket).file(audioName);
+        return updloadedAudioFile.getSignedUrl({
+            action: 'read',
+            expires: '03-09-2491'
+        }).then(signedUrls => {
+            audioPath = signedUrls[0];
+            console.log('audioPath is: ', signedUrls[0]);
+        });
+    })
+        .catch(error => {
+        console.error('Synthesize speech + write + upload error:', error);
+    });
+    console.log('about to save data to Firestore');
+    // Save Text to Firestore
+    const date = new Date().getTime();
+    const pageData = { text, pageName, date, audioPath };
+    docRef.add(pageData)
+        .then(() => {
+        // Delete image
+        gcs.bucket(fileBucket)
+            .file(pageName)
+            .delete()
+            .then(() => {
+            console.log(`gs://${fileBucket}/${pageName} deleted.`);
+        })
+            .catch(err => {
+            console.error('Delete image ERROR:', err);
+        });
+    }).catch((err) => {
+        console.log(err);
+    });
+    // Clean up temp directory
+    // return fse.remove(workingDir);
+    return null;
 }));
 //# sourceMappingURL=index.js.map
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+// Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireStorageModule } from 'angularfire2/storage';
@@ -17,25 +21,32 @@ import { DropZoneDirective } from './scan/drop-zone.directive';
 import { AppComponent } from './app.component';
 import { ListenComponent } from './listen/listen.component';
 import { ScanComponent } from './scan/scan.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ScanComponent,
-    DropZoneDirective,
-    ListenComponent,
-    ScanComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ScanComponent,
+        DropZoneDirective,
+        ListenComponent,
+        ScanComponent,
+        HeaderComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AngularFireStorageModule,
+        FormsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
